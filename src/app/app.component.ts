fbed3c1d7042a554f022a3aa5aa3e0c639e473ee
Nyanye htun <nyanyehtun@my.jcu.edu.au>
2018-12-08 02:10:09 +0800

    Remove test codes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,29 +9,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  item = new BaseCartItem({
-    id: 1,
-    name: 'Item 1',
-    price: 12.00,
-    image: 'http://img1.tebyan.net/Big/1393/01/20717721532161874817621852134711820022565.jpg',
-    quantity: 1
-  });
-
-  items = [new BaseCartItem({
-    id: 1,
-    name: 'Item 1',
-    price: 12.00,
-    image: 'http://img1.tebyan.net/Big/1393/01/20717721532161874817621852134711820022565.jpg',
-    quantity: 1
-  })];
-
-  settings: CheckoutHttpSettings = {
-    method: 'POST',
-    url: 'http://myapi.com/',
-    // options: { headers: { Authorization: 'Bearer my-auth-token' } }
-  };
-
-  public itemAdded(event) {
-    console.log(event);
-  }
 }
